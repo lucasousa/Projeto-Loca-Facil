f64f9fcb09f9cdb81888319e1a792c1c39ad513b
lucasousa <lucas103101@gmail.com>
2019-10-05 19:35:32 -0300

    Alterações para deixar Funcional

--- Telas/recuperar_login.py
+++ Telas/recuperar_login.py
@@ -10,7 +10,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
+class Ui_recuperar_login(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
