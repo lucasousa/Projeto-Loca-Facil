064d780b65f1c503be4a329767cfd5a56dad4c42
lucasousa <lucas103101@gmail.com>
2019-10-05 19:34:04 -0300

    Mudança de layout e adição de foto representativa

--- Telas/telainicial.py
+++ Telas/telainicial.py
@@ -18,7 +18,7 @@ class Ui_telainicial(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(30, 180, 731, 51))
+        self.label.setGeometry(QtCore.QRect(30, 70, 731, 51))
         font = QtGui.QFont()
         font.setFamily("Gadugi")
         font.setPointSize(20)
@@ -29,7 +29,7 @@ class Ui_telainicial(object):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(180, 310, 150, 60))
+        self.pushButton.setGeometry(QtCore.QRect(220, 391, 105, 45))
         font = QtGui.QFont()
         font.setFamily("Franklin Gothic Medium")
         font.setPointSize(10)
@@ -37,10 +37,11 @@ class Ui_telainicial(object):
         font.setWeight(75)
         self.pushButton.setFont(font)
         self.pushButton.setStyleSheet("color: rgb(255, 255, 255);\n"
-"background-color: rgb(52, 89, 149);\nborder-radius: 10px;\n")
+"background-color: rgb(52, 89, 149);\n"
+"border-radius: 15px;")
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(430, 310, 150, 60))
+        self.pushButton_2.setGeometry(QtCore.QRect(420, 391, 105, 45))
         font = QtGui.QFont()
         font.setFamily("Gadugi")
         font.setPointSize(10)
@@ -48,8 +49,18 @@ class Ui_telainicial(object):
         font.setWeight(75)
         self.pushButton_2.setFont(font)
         self.pushButton_2.setStyleSheet("color: rgb(255, 255, 255);\n"
-"background-color: rgb(52, 89, 149);\nborder-radius: 10px;\n")
+"background-color: rgb(52, 89, 149);\n"
+"border-radius: 15px;")
         self.pushButton_2.setObjectName("pushButton_2")
+        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton.setGeometry(QtCore.QRect(178, 140, 391, 231))
+        self.toolButton.setStyleSheet("width: 400px; height: 400px; border: none;")
+        self.toolButton.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("Imagens/house_blue.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.toolButton.setIcon(icon)
+        self.toolButton.setIconSize(QtCore.QSize(400, 400))
+        self.toolButton.setObjectName("toolButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -61,7 +72,7 @@ class Ui_telainicial(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Bem vindo ao sistema LocaFácil!"))
+        self.label.setText(_translate("MainWindow", "Bem vindo ao LocaFácil !"))
         self.pushButton.setText(_translate("MainWindow", "Cadastrar"))
         self.pushButton_2.setText(_translate("MainWindow", "Login"))
 
