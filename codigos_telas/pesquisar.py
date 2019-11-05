a03f5d3fdbfe3464ef24d1f9a1f823ebb991b63e
lucasousa <lucas103101@gmail.com>
2019-11-05 15:01:21 -0300

    Tela de pesquisar funcionando

--- codigos_telas/pesquisar.py
+++ codigos_telas/pesquisar.py
@@ -2,15 +2,27 @@
 
 # Form implementation generated from reading ui file 'pesquisar.ui'
 #
-# Created by: PyQt5 UI code generator 5.13.1
+# Created by: PyQt5 UI code generator 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
+    
 class Ui_Pesquisar(object):
+    def loadData(self, lista):
+        for row_number in range(len(lista)):
+            self.tableWidget.insertRow(row_number)
+            self.tableWidget.setItem(row_number, 0, QtWidgets.QTableWidgetItem(lista[row_number]['bairro']))
+            self.tableWidget.setItem(row_number, 1, QtWidgets.QTableWidgetItem(str(lista[row_number]['preco'])))
+            self.tableWidget.setItem(row_number, 2, QtWidgets.QTableWidgetItem(lista[row_number]['rua']))
+            btn = QtWidgets.QPushButton(self.tableWidget)
+            btn.setText("ver Informações")
+            btn.setStyleSheet("background-color: #345995;\n"
+"color: #FFFFFF;")
+            self.tableWidget.setCellWidget(row_number, 3, btn)
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(904, 600)
@@ -43,6 +55,11 @@ class Ui_Pesquisar(object):
         self.pushButton.setStyleSheet("background-color: #345995;\n"
 "color: #FFFFFF;")
         self.pushButton.setObjectName("pushButton")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(80, 150, 850, 450))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setRowCount(0)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -51,21 +68,13 @@ class Ui_Pesquisar(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
     
-    def add(self, lista):
-        self.listWidget.addItems(lista)
-        self.listar()
-    
-    def listar(self):
-        items = []
-        for i in range(self.listWidget.count()):
-            print(self.listWidget)
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.lineEdit.setPlaceholderText(_translate("MainWindow", "Digite o nome do bairro"))
         self.pushButton.setText(_translate("MainWindow", "Pesquisar"))
 
+   
 
 if __name__ == "__main__":
     import sys
